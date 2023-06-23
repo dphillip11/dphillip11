67280fbe6e1b1ff1c2d98cc991b45dd4dce34a35
Daniel <dphillip11@gmail.com>
2023-06-23 15:16:54 +0200

    colors

--- scripts.js
+++ scripts.js
@@ -1,88 +1,93 @@
 window.addEventListener('DOMContentLoaded', function () {
-    var iframeWrappers = document.querySelectorAll('.iframe-wrapper');
+  var projectNames = document.querySelectorAll('.project-name');
 
-    iframeWrappers.forEach(function (wrapper) {
-        var iframe = wrapper.querySelector('iframe');
+  projectNames.forEach(function (name) {
+    var dropdownContent = name.nextElementSibling;
 
-        wrapper.addEventListener('mouseenter', function () {
-            iframe.contentWindow.addEventListener('wheel', disableParentScroll, { passive: false });
-            iframe.contentWindow.addEventListener('keydown', disableArrowKeys, { passive: false });
-        });
-
-        wrapper.addEventListener('mouseleave', function () {
-            iframe.contentWindow.removeEventListener('wheel', disableParentScroll);
-            iframe.contentWindow.removeEventListener('keydown', disableArrowKeys);
-        });
-
-        function disableParentScroll(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            iframe.contentWindow.scrollBy(0, event.deltaY);
-        }
-
-        function disableArrowKeys(event) {
-            if (event.key.includes('Arrow')) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-        }
+    name.addEventListener('click', function () {
+      dropdownContent.classList.toggle('open');
+      adjustPageHeight();
     });
-});
+  });
 
-window.addEventListener('DOMContentLoaded', function () {
-    var projectNames = document.querySelectorAll('.project-name');
+function adjustPageHeight() {
+  var dropdowns = document.querySelectorAll('.dropdown-content.open');
+  var contactCard = document.querySelector('.contact-card');
+  var pageHeight = Math.max(
+    document.body.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.clientHeight,
+    document.documentElement.scrollHeight,
+    document.documentElement.offsetHeight
+  );
 
-    projectNames.forEach(function (name) {
-        var dropdownContent = name.nextElementSibling;
+  dropdowns.forEach(function (dropdown) {
+    pageHeight += dropdown.scrollHeight;
+  });
 
-        name.addEventListener('click', function () {
-            dropdownContent.classList.toggle('open');
-            adjustPageHeight();
-        });
-    });
+  if (contactCard) {
+    var contactCardOffset = contactCard.getBoundingClientRect().top + contactCard.offsetHeight;
+    pageHeight = Math.max(pageHeight, contactCardOffset + 20); // Adjusted by 20 pixels
+  }
 
-    function adjustPageHeight() {
-        var dropdowns = document.querySelectorAll('.dropdown-content.open');
-        var pageHeight = Math.max(
-            document.body.scrollHeight,
-            document.body.offsetHeight,
-            document.documentElement.clientHeight,
-            document.documentElement.scrollHeight,
-            document.documentElement.offsetHeight
-        );
-
-        dropdowns.forEach(function (dropdown) {
-            pageHeight += dropdown.scrollHeight;
-        });
-
-        document.body.style.height = pageHeight + 'px';
-    }
-});
-
-
- const carouselContainer = document.querySelector('.carousel-container');
-        const carouselImage = document.getElementById('carousel-image');
-        const carouselImages = document.querySelectorAll('.carousel-nav-item img');
-        let currentIndex = 0;
-
-        function handleNext() {
-            currentIndex = (currentIndex + 1) % carouselImages.length;
-            carouselImage.src = carouselImages[currentIndex].src;
+  document.body.style.height = pageHeight + 'px';
 }
-        function changeImage(e) {
-            handleNext();
-            const targetImageSrc = e.target.src;
-            carouselImage.src = targetImageSrc;
-        }
 
-        function handlePrev() {
-            currentIndex = (currentIndex - 1 + carouselImages.length) % carouselImages.length;
-            carouselImage.src = carouselImages[currentIndex].src;
-        }
+  var iframeWrappers = document.querySelectorAll('.iframe-wrapper');
 
-        carouselImages.forEach(image => {
-            image.addEventListener('mouseover', changeImage);
-        });
+  iframeWrappers.forEach(function (wrapper) {
+    var iframe = wrapper.querySelector('iframe');
 
+    wrapper.addEventListener('mouseenter', function () {
+      iframe.contentWindow.addEventListener('wheel', disableParentScroll, {
+        passive: false
+      });
+      iframe.contentWindow.addEventListener('keydown', disableArrowKeys, {
+        passive: false
+      });
+    });
 
+    wrapper.addEventListener('mouseleave', function () {
+      iframe.contentWindow.removeEventListener('wheel', disableParentScroll);
+      iframe.contentWindow.removeEventListener('keydown', disableArrowKeys);
+    });
 
+    function disableParentScroll(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      iframe.contentWindow.scrollBy(0, event.deltaY);
+    }
+
+    function disableArrowKeys(event) {
+      if (event.key.includes('Arrow')) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+    }
+  });
+
+  const carouselContainer = document.querySelector('.carousel-container');
+  const carouselImage = document.getElementById('carousel-image');
+  const carouselImages = document.querySelectorAll('.carousel-nav-item img');
+  let currentIndex = 0;
+
+  function handleNext() {
+    currentIndex = (currentIndex + 1) % carouselImages.length;
+    carouselImage.src = carouselImages[currentIndex].src;
+  }
+
+  function changeImage(e) {
+    handleNext();
+    const targetImageSrc = e.target.src;
+    carouselImage.src = targetImageSrc;
+  }
+
+  function handlePrev() {
+    currentIndex = (currentIndex - 1 + carouselImages.length) % carouselImages.length;
+    carouselImage.src = carouselImages[currentIndex].src;
+  }
+
+  carouselImages.forEach(image => {
+    image.addEventListener('mouseover', changeImage);
+  });
+});
